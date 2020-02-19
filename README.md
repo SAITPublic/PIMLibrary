884257fc31bf73c479b0689b8202f241ff8be179
Hyeonsu Kim <hyeonsu.kim@samsung.com>
2020-02-20 08:56:48 +0900

    update policy of external_libs
    1. add glog library in external_libs
    2. allow libraries and executables in .gitignore
    3. remove coding rule check for external_libs folder
    4. update README (how to build, how to run)
    
    Signed-off-by: Hyeonsu Kim <hyeonsu.kim@samsung.com>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FIMLibrary
 FIM Runtime Library and Tools
+
+# How to build
+
+project_root$ mkdir build
+project_root$ cd build
+project_root/build$ cmake ..
+project_root/build$ make -j8
+
+# How to run FimIntegrationTests
+
+project_root/build$ ./examples/FimIntegrationTests
--- scripts/check_format_all.sh
+++ scripts/check_format_all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find . -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs clang-format -i
+find runtime  -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs clang-format -i
+find examples -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs clang-format -i
+find tools    -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' | xargs clang-format -i
