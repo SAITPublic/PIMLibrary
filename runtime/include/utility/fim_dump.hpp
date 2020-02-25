8d52b4e55a13e9d0e31b85c15c076d001098c5aa
Hyeonsu Kim <hyeonsu.kim@samsung.com>
2020-02-25 14:36:03 +0900

    change memtrace dump format from ascii to hexa
    Signed-off-by: Hyeonsu Kim <hyeonsu.kim@samsung.com>

--- runtime/include/utility/fim_dump.hpp
+++ runtime/include/utility/fim_dump.hpp
@@ -50,7 +50,8 @@ int dump_fmtd16(const char* filename, FimMemTraceData* fmtd16, size_t fmtd16_siz
     for (int i = 0; i < fmtd16_size; i++) {
         fprintf(fp, "%d\t%d\t%d\t%c\t0x%lX", i, fmtd16[i].block_id, fmtd16[i].thread_id, fmtd16[i].cmd, fmtd16[i].addr);
         if (fmtd16[i].cmd == 'W') {
-            for (int d = 0; d < 16; d++) fprintf(fp, "%c", fmtd16[i].data[d]);
+            fprintf(fp, "\t");
+            for (int d = 0; d < 16; d++) fprintf(fp, "%02X", fmtd16[i].data[d]);
         }
         fprintf(fp, "\n");
     }
