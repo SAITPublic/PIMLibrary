a08b283e8493ca28be68b794fe32e3a473c2c8fe
Sudhakar M K <sudhakar.mk@samsung.com>
2020-03-05 13:21:49 +0000

    update trace converter
    - Remove file reading as command format changed
    - Add function for structure update
    
    Signed-off-by: Sudhakar M K <sudhakar.mk@samsung.com>

--- runtime/include/emulator/FimTraceCoalescer.h
+++ runtime/include/emulator/FimTraceCoalescer.h
@@ -16,59 +16,13 @@ namespace emulator
 // TODO: Define Stride for each FIM configuration
 #define TRANS_SIZE 0x10
 
-enum TraceType { MemRead, MemWrite, Barrier };
-typedef int64_t DATA;
-
-struct Cmd {
-    TraceType type_;
-    DATA data_;
-    char write_data_[65];
-    Cmd(TraceType t, DATA d, char *w_data)
-    {
-        type_ = t;
-        data_ = d;
-        if (w_data != NULL) {
-            int i = 0;
-            for (; w_data[i] != '\0'; i++) {
-                write_data_[i] = w_data[i];
-            }
-            write_data_[i] = '\0';
-        }
-    }
-
-    void append_data(const char *w_data)
-    {
-        int index = 0;
-        for (; write_data_[index] != '\0' && index < 32; index++) {
-            write_data_[index + 32] = write_data_[index];
-        }
-        write_data_[index + 32] = '\0';
-
-        if (w_data != NULL) {
-            int i = 0;
-            for (; w_data[i] != '\0'; i++) {
-                write_data_[i] = w_data[i];
-            }
-        }
-    }
-};
-
 class TraceParser
 {
    public:
-    void parse(std::string file_name);
-    void coalesce_traces();
-    DATA hex_to_int(char *str);
-    void print_hex_base(DATA addr);
-    std::vector<Cmd> &get_trace_data();
-    bool verify_coalesced_trace(std::vector<Cmd> verified_trace);
-
     void coalesce_trace(FimMemTraceData *fmtd32, int *fmtd32_size, FimMemTraceData *fmtd16, int fmtd16_size);
-    void append_data(uint8_t *dst, uint8_t *src, int size);
 
    private:
-    std::vector<Cmd> cur_vec_;
-    std::vector<Cmd> coalesced_mem_trace_;
+    void append_data(uint8_t *dst, uint8_t *src, int size);
 };
 
 } /* namespace emulator */
--- runtime/source/emulator/FimTraceCoalescer.cpp
+++ runtime/source/emulator/FimTraceCoalescer.cpp
@@ -6,35 +6,17 @@ namespace runtime
 {
 namespace emulator
 {
-void TraceParser::parse(std::string file_name)
-{
-    FILE *file_ptr = fopen(file_name.c_str(), "r");
-    if (file_ptr == NULL) {
-        std::cout << "Trace file" << file_name << " not found\n";
-        exit(1);
-    }
-    char type_str[10];
-    // TODO: bar_id for each channel
-    int bar_id = 0;
-    char addr[33], write_data[65];
-    DATA int_addr;
-    while (fscanf(file_ptr, "%s", type_str) > 0) {
-        if (!strcmp(type_str, "BAR")) {
-            cur_vec_.push_back(Cmd(Barrier, bar_id, NULL));
-        } else if (!strcmp(type_str, "READ")) {
-            fscanf(file_ptr, "%s", addr);
-            int_addr = hex_to_int(addr);
-            cur_vec_.push_back(Cmd(MemRead, int_addr, NULL));
-        } else if (!strcmp(type_str, "WRITE")) {
-            fscanf(file_ptr, "%s", addr);
-            int_addr = hex_to_int(addr);
-            fscanf(file_ptr, "%s", write_data);
-            cur_vec_.push_back(Cmd(MemWrite, int_addr, write_data));
-        }
-    }
-}
-
 void TraceParser::append_data(uint8_t *dst, uint8_t *src, int size) { memcpy(dst, src, size); }
+inline void coalesced_update(FimMemTraceData *fmtd32, int &coalesced_trace_it, FimMemTraceData *fmtd16, int &trace_it,
+                             char prev_cmd, uint64_t &prev_addr)
+{
+    fmtd32[coalesced_trace_it].cmd = prev_cmd;
+    fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
+    fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
+    fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
+    coalesced_trace_it++;
+    prev_addr = fmtd16[trace_it].addr;
+}
 
 void TraceParser::coalesce_trace(FimMemTraceData *fmtd32, int *fmtd32_size, FimMemTraceData *fmtd16, int fmtd16_size)
 {
@@ -54,179 +36,43 @@ void TraceParser::coalesce_trace(FimMemTraceData *fmtd32, int *fmtd32_size, FimM
                 break;
             case 'R':
                 if (prev_addr != -1 && prev_cmd == 'R') {
-                    if ((prev_addr + TRANS_SIZE) == fmtd16[trace_it].addr) {
+                    if ((prev_addr + TRANS_SIZE) == fmtd16[trace_it].addr)
                         ;
-                    } else {
-                        fmtd32[coalesced_trace_it].cmd = 'R';
-                        fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
-                        fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
-                        fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
-                        coalesced_trace_it++;
-                        prev_addr = fmtd16[trace_it].addr;
-                    }
+                    else
+                        coalesced_update(fmtd32, coalesced_trace_it, fmtd16, trace_it, 'R', prev_addr);
                 } else {
-                    fmtd32[coalesced_trace_it].cmd = 'R';
-                    fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
-                    fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
-                    fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
-                    coalesced_trace_it++;
-                    prev_addr = fmtd16[trace_it].addr;
+                    coalesced_update(fmtd32, coalesced_trace_it, fmtd16, trace_it, 'R', prev_addr);
                 }
                 prev_cmd = 'R';
                 break;
             case 'O':
                 if (prev_addr != -1 && prev_cmd == 'O') {
-                    if ((prev_addr + TRANS_SIZE) == fmtd16[trace_it].addr) {
+                    if ((prev_addr + TRANS_SIZE) == fmtd16[trace_it].addr)
                         ;
-                    } else {
-                        fmtd32[coalesced_trace_it].cmd = 'O';
-                        fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
-                        fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
-                        fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
-                        coalesced_trace_it++;
-                        prev_addr = fmtd16[trace_it].addr;
-                    }
+                    else
+                        coalesced_update(fmtd32, coalesced_trace_it, fmtd16, trace_it, 'O', prev_addr);
                 } else {
-                    fmtd32[coalesced_trace_it].cmd = 'O';
-                    fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
-                    fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
-                    fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
-                    coalesced_trace_it++;
-                    prev_addr = fmtd16[trace_it].addr;
+                    coalesced_update(fmtd32, coalesced_trace_it, fmtd16, trace_it, 'O', prev_addr);
                 }
                 prev_cmd = 'O';
                 break;
             case 'W':
                 if (prev_addr != -1 && prev_cmd == 'W') {
                     if ((prev_addr + TRANS_SIZE) == fmtd16[trace_it].addr) {
-                        for (int i = 0; i < 16; i++)
-                            append_data(fmtd32[coalesced_trace_it].data + 16, fmtd16[trace_it].data, 16);
+                        append_data(fmtd32[coalesced_trace_it - 1].data + 16, fmtd16[trace_it].data, 16);
                     } else {
-                        for (int i = 0; i < 16; i++) fmtd32[coalesced_trace_it].cmd = 'W';
-                        fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
-                        fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
-                        fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
                         memcpy(fmtd32[coalesced_trace_it].data, fmtd16[trace_it].data, 16);
-                        coalesced_trace_it++;
-                        prev_addr = fmtd16[trace_it].addr;
+                        coalesced_update(fmtd32, coalesced_trace_it, fmtd16, trace_it, 'W', prev_addr);
                     }
                 } else {
-                    for (int i = 0; i < 16; i++) fmtd32[coalesced_trace_it].cmd = 'W';
-                    fmtd32[coalesced_trace_it].block_id = fmtd16[trace_it].block_id;
-                    fmtd32[coalesced_trace_it].thread_id = fmtd16[trace_it].thread_id;
-                    fmtd32[coalesced_trace_it].addr = fmtd16[trace_it].addr;
                     memcpy(fmtd32[coalesced_trace_it].data, fmtd16[trace_it].data, 16);
-                    coalesced_trace_it++;
-                    prev_addr = fmtd16[trace_it].addr;
+                    coalesced_update(fmtd32, coalesced_trace_it, fmtd16, trace_it, 'W', prev_addr);
                 }
                 prev_cmd = 'W';
                 break;
         }
     }
     fmtd32_size[0] = coalesced_trace_it;
-}
-
-void TraceParser::coalesce_traces()
-{
-    coalesced_mem_trace_.clear();
-    DATA prev_addr;
-    TraceType prev_cmd = Barrier;
-    for (int trace_it = 0; trace_it < (int)cur_vec_.size(); trace_it++) {
-        switch (cur_vec_[trace_it].type_) {
-            case Barrier:
-                coalesced_mem_trace_.push_back(Cmd(cur_vec_[trace_it]));
-                prev_addr = -1;
-                prev_cmd = Barrier;
-                break;
-            case MemRead:
-                if (prev_addr != -1 && prev_cmd == MemRead) {
-                    if ((prev_addr + TRANS_SIZE) == cur_vec_[trace_it].data_) {
-                        ;
-                    } else {
-                        coalesced_mem_trace_.push_back(Cmd(cur_vec_[trace_it]));
-                        prev_addr = cur_vec_[trace_it].data_;
-                    }
-                } else {
-                    coalesced_mem_trace_.push_back(Cmd(cur_vec_[trace_it]));
-                    prev_addr = cur_vec_[trace_it].data_;
-                }
-                prev_cmd = MemRead;
-                break;
-            case MemWrite:
-                if (prev_addr != -1 && prev_cmd == MemWrite) {
-                    if ((prev_addr + TRANS_SIZE) == cur_vec_[trace_it].data_) {
-                        coalesced_mem_trace_.back().append_data(cur_vec_[trace_it].write_data_);
-                    } else {
-                        coalesced_mem_trace_.push_back(Cmd(cur_vec_[trace_it]));
-                        prev_addr = cur_vec_[trace_it].data_;
-                    }
-                } else {
-                    coalesced_mem_trace_.push_back(Cmd(cur_vec_[trace_it]));
-                    prev_addr = cur_vec_[trace_it].data_;
-                }
-                prev_cmd = MemWrite;
-                break;
-        }
-    }
-}
-
-DATA TraceParser::hex_to_int(char *str)
-{
-    if (str == NULL || str[0] == '\0' || str[1] == '\0' || str[2] == '\0') return 0;
-    if (str[0] != '0' || str[1] != 'x') return 0;
-    int i = 2;
-    DATA ret = 0;
-    while (str[i] != '\0') {
-        if (str[i] >= '0' && str[i] <= '9') {
-            ret = (ret * 16) + (str[i] - '0');
-        } else if (str[i] >= 'a' && str[i] <= 'f') {
-            ret = (ret * 16) + (str[i] - 'a' + 10);
-        }
-        i++;
-    }
-    return ret;
-}
-
-std::vector<Cmd> &TraceParser::get_trace_data() { return cur_vec_; }
-
-bool TraceParser::verify_coalesced_trace(std::vector<Cmd> verified_trace)
-{
-    if (coalesced_mem_trace_.size() != verified_trace.size()) {
-        std::cout << "Size mismatch\n";
-        return false;
-    }
-
-    for (int i = 0; i < (int)coalesced_mem_trace_.size(); i++) {
-        if (coalesced_mem_trace_[i].type_ != verified_trace[i].type_) {
-            std::cout << "Trace file Cmd  mismatched at point. Expected " << verified_trace[i].type_ << " got "
-                      << coalesced_mem_trace_[i].type_ << "\n";
-            return false;
-        }
-        if (coalesced_mem_trace_[i].type_ == Barrier) {
-            if (coalesced_mem_trace_[i].data_ != verified_trace[i].data_) {
-                std::cout << "Trace file Barrier Channel mismatched at point. Expected " << verified_trace[i].data_
-                          << " got " << coalesced_mem_trace_[i].data_ << "\n";
-                return false;
-            }
-        } else {
-            if (coalesced_mem_trace_[i].data_ != verified_trace[i].data_) {
-                std::cout << "Trace file MemRead/Write Address mismatched at point. Expected "
-                          << verified_trace[i].data_ << " got " << coalesced_mem_trace_[i].data_ << "\n";
-                return false;
-            }
-            if (coalesced_mem_trace_[i].type_ == MemWrite) {
-                for (int dataIt = 0; coalesced_mem_trace_[i].write_data_[dataIt] != '\0'; dataIt++) {
-                    if (coalesced_mem_trace_[i].write_data_[dataIt] != verified_trace[i].write_data_[dataIt]) {
-                        std::cout << "Trace file MemWrite data mismatched at point. Expected "
-                                  << verified_trace[i].write_data_[dataIt] << " got "
-                                  << coalesced_mem_trace_[i].write_data_[dataIt] << "\n";
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-    return true;
 }
 
 } /* namespace emulator */
