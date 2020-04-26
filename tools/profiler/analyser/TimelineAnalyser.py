2cd15db4f484811edfc0cc521cfe6eeacc0bbf32
u.chugh <u.chugh@samsung.com>
2020-04-26 21:03:44 +0530

    Modify TimelineAnalyser to add parameter
    - Add name_col parameter to TimelineAnalyser Module
    
    Signed-off-by: Ujjawal Chugh <u.chugh@samsung.com>

--- tools/profiler/analyser/TimelineAnalyser.py
+++ tools/profiler/analyser/TimelineAnalyser.py
@@ -1,22 +1,23 @@
-def get_start_end_times(df, start_col_name = 'BeginNs', end_col_name = 'EndNs'):
+def get_start_end_times(df, name_col='KernelName', start_col = 'BeginNs', end_col = 'EndNs'):
 	'''Function to extract start and end Times of events from pandas dataframe
 		df = Pandas dataframe contaning data
-		start_col_name = Name of column contaning start times
-		end_col_name = Name of column contaning end times
+		name_col = Column containg name of events to be plotted (KernelName,ModuleName,APIName, etc)
+		start_col = Column contaning start times
+		end_col = Column contaning end times
 		Returns list of event names, start_times and end_times
 	'''
 	event_start_times = []
 	event_end_times = []
 	event_names = []
 
-	name_groups = df.groupby('KernelName')
+	name_groups = df.groupby(name_col)
 
 	for name, group in name_groups:
 		event_names.append(name[0:20])
-		start_list = group[start_col_name].tolist()
+		start_list = group[start_col].tolist()
 		start_list = [int(x) for x in start_list]
 		event_start_times.append(start_list)
-		end_list = group[end_col_name].tolist()
+		end_list = group[end_col].tolist()
 		end_list = [int(x) for x in end_list]
 		event_end_times.append(end_list)
 
