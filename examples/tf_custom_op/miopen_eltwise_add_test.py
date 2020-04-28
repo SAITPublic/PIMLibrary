d4546cab15450e2d6b42d0499fc17911de06c1fb
Won-Jo Lee <w-j.lee@samsung.com>
2020-04-28 10:22:56 +0900

    Add test coverage for TF eltwise-add
      - Eltwise-add loop test to cover a number of channel/width/height combinations.

--- examples/tf_custom_op/miopen_eltwise_add_test.py
+++ examples/tf_custom_op/miopen_eltwise_add_test.py
@@ -29,21 +29,42 @@ class MIopenAddTestConstant(tf.test.TestCase):
         result = miopen_elt(t_input0,t_input1,add)
         self.assertAllEqual(result, [51.,7.,8.,9.,10.])
 
+
 class MIopenAddTestRandom(tf.test.TestCase):
 
-  def test(self):
-      with self.test_session():
+    def test(self):
+        # Todo: Should test scholar add after FIM Kernel supported
+        input0_dim = 4
+        input1_dim = [4, 1]
+        batch_size = [1, 4, 8]
+        channel = 1
+        width = [128, 256, 384]
+        height = [768, 1024]
+
+        for i in batch_size:
+            for j in width:
+                for k in height:
+                    with self.test_session():1
+                        #FIXME: minval, maxval would be changed to finfo(np.float16).min & max
+                        input0 = tf.random.uniform(shape=[i, channel, j, k], minval=-300.0, maxval=300.0, dtype=np.float16)
+                        input1 = tf.random.uniform(shape=[i, channel, j, k], minval=-300.0, maxval=300.0, dtype=np.float16)
+                        add = tf.constant([0], dtype=np.int32)
+
+                        result_custom = miopen_elt(input0, input1, add)
+                        result_math_add = tf.math.add(input0, input1)
+                        self.assertAllEqual(result_custom, result_math_add)
+
         input0 = tf.random.uniform(shape=[65536], minval=0, maxval=64, dtype = np.float16)
         t_input0 = tf.convert_to_tensor(input0, np.float16)
-
         input1 = tf.random.uniform(shape=[65536], minval=0, maxval=64, dtype = np.float16)
         t_input1   = tf.convert_to_tensor(input1, np.float16)
-
         add = tf.constant([0],dtype = np.int32)
-        result_custom = miopen_elt(t_input0, t_input1,add)
+
+        result_custom = miopen_elt(t_input0, t_input1, add)
         result_math_add = tf.math.add(t_input0, t_input1)
 
         self.assertAllEqual(result_custom, result_math_add)
+
 
 class MIopenAddTestFile(tf.test.TestCase):
 
@@ -61,7 +82,7 @@ class MIopenAddTestFile(tf.test.TestCase):
         golden  =  np.fromfile("../test_vectors/load/elt_add/output_128KB.dat", dtype = np.float16)
         self.assertAllEqual(result, golden)
 
+
 if __name__ == '__main__':
   tf.test.main()
-
 
