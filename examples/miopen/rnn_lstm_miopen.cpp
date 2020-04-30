3c7f222667a915636e9a2250f6b298f5a64329dd
Rahul Shrivastava <r.shrivastav@samsung.com>
2020-04-30 11:01:32 +0000

    Initialize vectors with random values
    -Add code to initialize the device vectors with random fp16 values.
    
    Signed-off-by: Rahul Shrivastava <r.shrivastav@samsung.com>

--- examples/miopen/rnn_lstm_miopen.cpp
+++ examples/miopen/rnn_lstm_miopen.cpp
@@ -29,15 +29,14 @@ int miopen_rnn_lstm()
     miopenCreateTensorDescriptor(&output_tensor);
     miopenCreateRNNDescriptor(&rnnDesc);
 
-    int nseq = 10;                                          // Number of iterations to unroll over
-    int in_h = 1024;                                        // Input Length
+    int nseq = 10;                                            // Number of iterations to unroll over
+    int in_h = 1024;                                          // Input Length
     std::vector<int> in_len({4, 4, 4, 3, 3, 3, 2, 2, 2, 1});  // input tensor length
     in_len.push_back(in_h);
 
     int hid_h = 512;                           // Hidden State Length
     int hid_l = 3 * 2;                         // Number of hidden stacks, *2 for bidirection
     std::vector<int> hid_len({hid_l, hid_h});  // hidden tensor length
-
 
     int out_h = hid_h * 2;              // for bidirection
     std::vector<int> out_len({out_h});  // output tensor length
@@ -57,8 +56,8 @@ int miopen_rnn_lstm()
     std::array<int, 3> hid_lens = {{hid_len[0], in_len[0], hid_len[1]}};
     miopenSetTensorDescriptor(hidden_tensor, miopenHalf, 3, hid_lens.data(), nullptr);
 
-    int layer = 3;  // Number of hidden stacks
-    int wei_hh = 512;   // Hidden state length
+    int layer = 3;     // Number of hidden stacks
+    int wei_hh = 512;  // Hidden state length
     miopenRNNMode_t mode = miopenLSTM;
     miopenRNNBiasMode_t biasMode = miopenRNNNoBias;
     miopenRNNDirectionMode_t directionMode;
@@ -96,10 +95,70 @@ int miopen_rnn_lstm()
     hipMalloc(&hy_dev, sizeof(half) * hy_sz);
     hipMalloc(&cy_dev, sizeof(half) * hy_sz);
 
-    miopenRNNForwardInference(handle, rnnDesc, nseq, input_tensors.data(), in_dev, hidden_tensor,
-                              hx_dev, hidden_tensor, cx_dev, weight_tensor, wei_dev,
-                              output_tensors.data(), out_dev, hidden_tensor, hy_dev, hidden_tensor,
-                              cy_dev, workspace_dev, workSpace_sz * sizeof(half));
+    /******************************
+    Initialization begin
+    *****************************/
+
+    std::vector<half> in;
+    std::vector<half> wei;
+    std::vector<half> out;
+    std::vector<half> hx;
+    std::vector<half> cx;
+    std::vector<half> hy;
+    std::vector<half> cy;
+    std::vector<half> workspace;
+    std::vector<double> outhost;
+    std::vector<double> hy_host;
+    std::vector<double> cy_host;
+
+    in = std::vector<half>(in_sz);
+    hx = std::vector<half>(hy_sz, static_cast<half>(0));
+    wei = std::vector<half>(wei_sz);
+    out = std::vector<half>(out_sz, static_cast<half>(0));
+    cx = std::vector<half>(hy_sz, static_cast<half>(0));
+    hy = std::vector<half>(hy_sz, static_cast<half>(0));
+    cy = std::vector<half>(hy_sz, static_cast<half>(0));
+    hy_host = std::vector<double>(hy_sz, static_cast<double>(0));
+    cy_host = std::vector<double>(hy_sz, static_cast<double>(0));
+
+    workspace = std::vector<half>(workSpace_sz, static_cast<half>(0));
+    outhost = std::vector<double>(out_sz, static_cast<double>(0));
+
+    srand(0);
+    double scale = 0.01;
+
+    for (int i = 0; i < in_sz; i++) {
+        in[i] = static_cast<half>((static_cast<double>(scale * rand()) * (1.0 / RAND_MAX)));
+    }
+
+    for (int i = 0; i < hy_sz; i++) {
+        hx[i] = static_cast<half>((scale * static_cast<double>(rand()) * (1.0 / RAND_MAX)));
+    }
+
+    for (int i = 0; i < hy_sz; i++) {
+        cx[i] = static_cast<half>((scale * static_cast<double>(rand()) * (1.0 / RAND_MAX)));
+    }
+
+    for (int i = 0; i < wei_sz; i++) {
+        wei[i] = static_cast<half>((scale * static_cast<double>((rand()) * (1.0 / RAND_MAX) - 0.5)));
+    }
+
+    hipMemcpy(in_dev, in.data(), sizeof(half) * in_sz, hipMemcpyHostToDevice);
+    hipMemcpy(wei_dev, wei.data(), sizeof(half) * wei_sz, hipMemcpyHostToDevice);
+    hipMemcpy(out_dev, out.data(), sizeof(half) * out_sz, hipMemcpyHostToDevice);
+    hipMemcpy(cx_dev, cx.data(), sizeof(half) * hy_sz, hipMemcpyHostToDevice);
+    hipMemcpy(hx_dev, hx.data(), sizeof(half) * hy_sz, hipMemcpyHostToDevice);
+    hipMemcpy(workspace_dev, workspace.data(), sizeof(half) * workSpace_sz, hipMemcpyHostToDevice);
+    hipMemcpy(hy_dev, hy.data(), sizeof(half) * hy_sz, hipMemcpyHostToDevice);
+    hipMemcpy(cy_dev, cy.data(), sizeof(half) * hy_sz, hipMemcpyHostToDevice);
+
+    /******************************
+    Initialization end
+    *****************************/
+
+    miopenRNNForwardInference(handle, rnnDesc, nseq, input_tensors.data(), in_dev, hidden_tensor, hx_dev, hidden_tensor,
+                              cx_dev, weight_tensor, wei_dev, output_tensors.data(), out_dev, hidden_tensor, hy_dev,
+                              hidden_tensor, cy_dev, workspace_dev, workSpace_sz * sizeof(half));
 
     miopenDestroyTensorDescriptor(output_tensor);
     miopenDestroyTensorDescriptor(weight_tensor);
