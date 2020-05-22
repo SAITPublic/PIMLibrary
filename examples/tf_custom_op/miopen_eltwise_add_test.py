9d1333132ac48456c512bbcf7186d1532a9cd3e9
Won-Jo Lee <w-j.lee@samsung.com>
2020-05-22 04:27:13 +0000

    Add test coverage loop for Tensorflow Unittest
      - Uniittest for Eltwise add & Mul.
      - Add a variation of test coverages.
      - It Shows what are failed.
    
    Signed-off-by: Won-Jo Lee <w-j.lee@samsung.com>

--- examples/tf_custom_op/miopen_eltwise_add_test.py
+++ examples/tf_custom_op/miopen_eltwise_add_test.py
@@ -5,11 +5,10 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.python.platform import test
 try:
-  from tf_fim_ops.python.ops.miopen_eltwise_ops import miopen_elt
+    from tf_fim_ops.python.ops.miopen_eltwise_ops import miopen_elt
 except ImportError:
-  from miopen_eltwise_ops import miopen_elt
+    from miopen_eltwise_ops import miopen_elt
 
 tf.debugging.set_log_device_placement(True)
 testFilesPath = '../test_vectors/'
@@ -17,72 +16,91 @@ testFilesPath = '../test_vectors/'
 
 class MIopenAddTestConstant(tf.test.TestCase):
 
-  def test(self):
-      with self.test_session():
-        input0  =  tf.constant([1.,2.,3.,4.,5.],dtype = np.float16)
-        t_input0   = tf.convert_to_tensor(input0, np.float16)
+    def test(self):
+        with self.test_session():
+            input0 = tf.constant([1.,2.,3.,4.,5.], dtype=np.float16)
+            input1 = tf.constant([50.,5.,5.,5.,5.], dtype=np.float16)
 
-        input1  =  tf.constant([50.,5.,5.,5.,5.],dtype = np.float16)
-        t_input1   = tf.convert_to_tensor(input1, np.float16)
-
-        add = tf.constant([0],dtype = np.int32)
-        result = miopen_elt(t_input0,t_input1,add)
-        self.assertAllEqual(result, [51.,7.,8.,9.,10.])
+            add = tf.constant([0], dtype=np.int32)
+            result = miopen_elt(input0, input1, add)
+            self.assertAllEqual(result, [51.,7.,8.,9.,10.])
 
 
 class MIopenAddTestRandom(tf.test.TestCase):
 
-    def test(self):
-        # Todo: Should test scholar add after FIM Kernel supported
-        input0_dim = 4
-        input1_dim = [4, 1]
+    def test_4dim_4dim(self):
         batch_size = [1, 4, 8]
-        channel = 1
+        channel = [1]
         width = [128, 256, 384]
         height = [768, 1024]
+        failed_cases = []
+        success = True
 
-        for i in batch_size:
-            for j in width:
-                for k in height:
-                    with self.test_session():
-                        #FIXME: minval, maxval would be changed to finfo(np.float16).min & max
-                        input0 = tf.random.uniform(shape=[i, channel, j, k], minval=-300.0, maxval=300.0, dtype=np.float16)
-                        input1 = tf.random.uniform(shape=[i, channel, j, k], minval=-300.0, maxval=300.0, dtype=np.float16)
-                        add = tf.constant([0], dtype=np.int32)
+        for b in batch_size:
+            for c in channel:
+                for w in width:
+                    for h in height:
+                        with self.test_session():
+                            input0 = tf.random.uniform(shape=[b, c, w, h], minval=(np.finfo(np.float16).min / 2),
+                                                       maxval=(np.finfo(np.float16).max / 2), dtype=np.float16)
+                            input1 = tf.random.uniform(shape=[b, c, w, h], minval=(np.finfo(np.float16).min / 2),
+                                                       maxval=(np.finfo(np.float16).max / 2), dtype=np.float16)
+                            add = tf.constant([0], dtype=np.int32)
 
-                        result_custom = miopen_elt(input0, input1, add)
-                        result_math_add = tf.math.add(input0, input1)
-                        self.assertAllEqual(result_custom, result_math_add)
+                            result_custom = miopen_elt(input0, input1, add)
+                            result_math_add = tf.math.add(input0, input1)
+                            try:
+                                self.assertAllEqual(result_custom, result_math_add)
+                            except Exception as ex:
+                                failed_cases.append([b, c, w, h])
+                                success = False
+        print("Test cases failed!: " + str(failed_cases))
+        self.assertEqual(success, True)
 
-        input0 = tf.random.uniform(shape=[65536], minval=0, maxval=64, dtype = np.float16)
-        t_input0 = tf.convert_to_tensor(input0, np.float16)
-        input1 = tf.random.uniform(shape=[65536], minval=0, maxval=64, dtype = np.float16)
-        t_input1   = tf.convert_to_tensor(input1, np.float16)
-        add = tf.constant([0],dtype = np.int32)
+    def test_4im_scalar(self):
+        batch_size = [1, 8]
+        channel = [1]
+        width = [128]
+        height = [768, 1024]
+        failed_cases = []
+        success = True
 
-        result_custom = miopen_elt(t_input0, t_input1, add)
-        result_math_add = tf.math.add(t_input0, t_input1)
+        for b in batch_size:
+            for c in channel:
+                for w in width:
+                    for h in height:
+                        with self.test_session():
+                            input0 = tf.random.uniform(shape=[b, c, w, h], minval=(np.finfo(np.float16).min / 2),
+                                                       maxval=(np.finfo(np.float16).max / 2), dtype=np.float16)
+                            input1 = tf.constant(np.random.randint(np.finfo(np.float16).min / 2,
+                                                                   np.finfo(np.float16).max / 2), dtype=np.float16)
 
-        self.assertAllEqual(result_custom, result_math_add)
+                            add = tf.constant([0], dtype=np.int32)
+
+                            result_custom = miopen_elt(input0, input1, add)
+                            result_math_add = tf.math.add(input0, input1)
+                            try:
+                                self.assertAllEqual(result_custom, result_math_add)
+                            except Exception as ex:
+                                failed_cases.append([b, c, w, h])
+                                success = False
+        print("Test cases failed!: " + str(failed_cases))
+        self.assertEqual(success, True)
 
 
 class MIopenAddTestFile(tf.test.TestCase):
 
-  def test(self):
-      with self.test_session():
+    def test(self):
+        with self.test_session():
+            input0 = np.fromfile("../test_vectors/load/elt_add/input0_128KB.dat", dtype=np.float16)
+            input1 = np.fromfile("../test_vectors/load/elt_add/input1_128KB.dat", dtype=np.float16)
 
-        input0  =  np.fromfile("../test_vectors/load/elt_add/input0_128KB.dat", dtype = np.float16)
-        t_input0   = tf.convert_to_tensor(input0, np.float16)
-
-        input1  =  np.fromfile("../test_vectors/load/elt_add/input1_128KB.dat", dtype = np.float16)
-        t_input1   = tf.convert_to_tensor(input1, np.float16)
-
-        add = tf.constant([0],dtype = np.int32)
-        result = miopen_elt(t_input0,t_input1,add)
-        golden  =  np.fromfile("../test_vectors/load/elt_add/output_128KB.dat", dtype = np.float16)
-        self.assertAllEqual(result, golden)
+            add = tf.constant([0], dtype=np.int32)
+            result = miopen_elt(input0, input1, add)
+            golden = np.fromfile("../test_vectors/load/elt_add/output_128KB.dat", dtype=np.float16)
+            self.assertAllEqual(result, golden)
 
 
 if __name__ == '__main__':
-  tf.test.main()
+    tf.test.main()
 
--- examples/tf_custom_op/miopen_eltwise_mul_test.py
+++ examples/tf_custom_op/miopen_eltwise_mul_test.py
@@ -5,61 +5,101 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.python.platform import test
 try:
-  from tf_fim_ops.python.ops.miopen_eltwise_ops import miopen_elt
+    from tf_fim_ops.python.ops.miopen_eltwise_ops import miopen_elt
 except ImportError:
-  from miopen_eltwise_ops import miopen_elt
+    from miopen_eltwise_ops import miopen_elt
 
 tf.debugging.set_log_device_placement(True)
 testFilesPath = '../test_vectors/'
 
 
-
 class MIopenMulTestConstant(tf.test.TestCase):
 
-  def test(self):
-      with self.test_session():
-        input0  =  tf.constant([1.,2.,3.,4.,5.],dtype = np.float16)
-        t_input0   = tf.convert_to_tensor(input0, np.float16)
+    def test(self):
+        with self.test_session():
+            input0 = tf.constant([1., 2., 3., 4., 5.], dtype=np.float16)
+            input1 = tf.constant([50., 5., 5., 5., 0.], dtype=np.float16)
 
-        input1  =  tf.constant([50.,5.,5.,5.,0.],dtype = np.float16)
-        t_input1   = tf.convert_to_tensor(input1, np.float16)
+            mul = tf.constant([1], dtype=np.int32)
+            result = miopen_elt(input0, input1, mul)
+            self.assertAllEqual(result, [50., 10., 15., 20., 0.])
 
-        mul = tf.constant([1],dtype = np.int32)
-        result = miopen_elt(t_input0,t_input1,mul)
-        self.assertAllEqual(result, [50.,10.,15.,20.,0.])
 
 class MIopenMulTestRandom(tf.test.TestCase):
 
-  def test(self):
-      with self.test_session():
-        input0 = tf.random.uniform(shape=[65536], minval=0, maxval=64, dtype = np.float16)
-        t_input0 = tf.convert_to_tensor(input0, np.float16)
+    def test_4dim_4dim(self):
+        batch_size = [1, 4, 8]
+        channel = [1, 12]
+        width = [128, 256, 384]
+        height = [768, 1024]
+        failed_cases = []
+        success = True
 
-        input1 = tf.random.uniform(shape=[65536], minval=0, maxval=64, dtype = np.float16)
-        t_input1   = tf.convert_to_tensor(input1, np.float16)
+        for b in batch_size:
+            for c in channel:
+                for w in width:
+                    for h in height:
+                        with self.test_session():
+                            input0 = tf.random.uniform(shape=[b, c, w, h], minval=-np.sqrt(np.finfo(np.float16).max),
+                                                       maxval=np.sqrt(np.finfo(np.float16).max), dtype=np.float16)
+                            input1 = tf.random.uniform(shape=[b, c, w, h], minval=-np.sqrt(np.finfo(np.float16).max),
+                                                       maxval=np.sqrt(np.finfo(np.float16).max), dtype=np.float16)
+                            mul = tf.constant([1], dtype=np.int32)
 
-        mul = tf.constant([1],dtype = np.int32)
-        result_custom = miopen_elt(t_input0, t_input1,mul)
-        result_math_add = tf.math.multiply(t_input0, t_input1)
+                            result_custom = miopen_elt(input0, input1, mul)
+                            result_math_multiply = tf.math.multiply(input0, input1)
+                            try:
+                                self.assertAllEqual(result_custom, result_math_multiply)
+                            except Exception as ex:
+                                failed_cases.append([b, c, w, h])
+                                success = False
+        print("Test cases failed!: " + str(failed_cases))
+        self.assertEqual(success, True)
 
-        self.assertAllEqual(result_custom, result_math_add)
+    def test_4im_scalar(self):
+        batch_size = [1, 8]
+        channel = [12]
+        width = [128]
+        height = [384]
+        failed_cases = []
+        success = True
+
+        for b in batch_size:
+            for c in channel:
+                for w in width:
+                    for h in height:
+                        with self.test_session():
+                            input0 = tf.random.uniform(shape=[b, c, w, h], minval=-np.sqrt(np.finfo(np.float16).max),
+                                                       maxval=np.sqrt(np.finfo(np.float16).max), dtype=np.float16)
+                            input1 = tf.constant(np.random.randint(-np.sqrt(np.finfo(np.float16).max),
+                                                                   np.sqrt(np.finfo(np.float16).max), dtype=np.float16))
+
+                            mul = tf.constant([1], dtype=np.int32)
+
+                            result_custom = miopen_elt(input0, input1, mul)
+                            result_math_multiply = tf.math.multiply(input0, input1)
+                            try:
+                                self.assertAllEqual(result_custom, result_math_multiply)
+                            except Exception as ex:
+                                failed_cases.append([b, c, w, h])
+                                success = False
+        print("Test cases failed!: " + str(failed_cases))
+        self.assertEqual(success, True)
+
 
 class MIopenMulTestFile(tf.test.TestCase):
 
-  def test(self):
-      with self.test_session():
-        input0  =  np.fromfile("../test_vectors/load/elt_mul/input0_128KB.dat", dtype = np.float16)
-        t_input0   = tf.convert_to_tensor(input0, np.float16)
+    def test(self):
+        with self.test_session():
+            input0 = np.fromfile("../test_vectors/load/elt_mul/input0_128KB.dat", dtype=np.float16)
+            input1 = np.fromfile("../test_vectors/load/elt_mul/input1_128KB.dat", dtype=np.float16)
 
-        input1  =  np.fromfile("../test_vectors/load/elt_mul/input1_128KB.dat", dtype = np.float16)
-        t_input1   = tf.convert_to_tensor(input1, np.float16)
+            mul = tf.constant([1], dtype=np.int32)
+            result = miopen_elt(input0, input1, mul)
+            golden = np.fromfile("../test_vectors/load/elt_mul/output_128KB.dat", dtype = np.float16)
+            self.assertAllEqual(result, golden)
 
-        mul = tf.constant([1],dtype = np.int32)
-        result = miopen_elt(t_input0,t_input1,mul)
-        golden  =  np.fromfile("../test_vectors/load/elt_mul/output_128KB.dat", dtype = np.float16)
-        self.assertAllEqual(result, golden)
 
 if __name__ == '__main__':
   tf.test.main()
