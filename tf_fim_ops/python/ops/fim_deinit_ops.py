8cb0cfef1cf3843c3ac18d90816a2468016d9984
Won-Jo Lee <w-j.lee@samsung.com>
2020-06-18 01:29:39 +0000

    Clean fim_init & deinit code
      - remove print
      - remove unused import
    
    Signed-off-by: Won-Jo Lee <w-j.lee@samsung.com>

--- tf_fim_ops/python/ops/fim_deinit_ops.py
+++ tf_fim_ops/python/ops/fim_deinit_ops.py
@@ -4,6 +4,5 @@ from __future__ import print_function
 
 from tensorflow.python.framework import load_library
 
-fim_deinit_ops = load_library.load_op_library(
-    ('libfim_deinit.so'))
+fim_deinit_ops = load_library.load_op_library(('libfim_deinit.so'))
 fim_deinit = fim_deinit_ops.fim_deinit
