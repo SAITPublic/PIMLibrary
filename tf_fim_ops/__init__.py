a2165c53d6359b6e3caf48fa3829b3414cbb59af
sundeep.k <sundeep.k@samsung.com>
2020-07-15 06:03:35 +0000

    Remove miopen ops
    Signed-off-by: sundeep.k <sundeep.k@samsung.com>

--- tf_fim_ops/__init__.py
+++ tf_fim_ops/__init__.py
@@ -1,8 +1,5 @@
 from __future__ import absolute_import
 
-from tf_fim_ops.python.ops.miopen_eltwise_ops import miopen_elt
-from tf_fim_ops.python.ops.miopen_activation_ops import miopen_act
-from tf_fim_ops.python.ops.miopen_bn_ops import miopen_bn
 from tf_fim_ops.python.ops.fim_gemv_ops import fim_gemv
 from tf_fim_ops.python.ops.fim_bn_ops import fim_bn
 from tf_fim_ops.python.ops.fim_activation_ops import fim_act
