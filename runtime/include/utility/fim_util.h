33a0f0457e5bb87cc7a25f785acd6900abcc61b9
Shashidhar G <shashidhar.g@samsung.com>
2020-07-24 12:19:03 +0000

    Added elt_wise_op kernel with 64CU and 16 threads
    Run elt_wise op kernel on 64CUs each with 16 threads.
    
    Signed-off-by: Shashidhar G <shashidhar.g@samsung.com>

--- runtime/include/utility/fim_util.h
+++ runtime/include/utility/fim_util.h
@@ -108,6 +108,9 @@ __device__ void compute_elt_op_1cu_2th(volatile uint8_t* __restrict__ fim_input0
                                        volatile uint8_t* __restrict__ fim_input1,
                                        volatile uint8_t* __restrict__ fim_output, int num_tile, uint64_t offset);
 
+__device__ void compute_elt_op_64cu_16th(volatile uint8_t* __restrict__ fim_input0,
+                                       volatile uint8_t* __restrict__ fim_input1,
+                                       volatile uint8_t* __restrict__ fim_output, int num_tile, uint64_t offset);
 /* 64CU 2TH functions */
 
 __device__ void add_transaction_all_64cu_2th(volatile uint8_t* __restrict__ fim_addr, bool is_write, uint32_t bg,
