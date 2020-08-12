d9f26edb68e00ebda6b3c37531cb894d71efe6c5
Sudhakar M K <sudhakar.mk@samsung.com>
2020-08-12 04:50:03 +0000

    Add profile test case for fim add and mul
    This commit adds an profile test case for Fim Add And Mul
    
    Signed-off-by: Sudhakar M K <sudhakar.mk@samsung.com>

--- examples/hip/fim_elt_add.cpp
+++ examples/hip/fim_elt_add.cpp
@@ -7,6 +7,7 @@
 #include "fim_runtime_api.h"
 #include "hip/hip_fp16.h"
 #include "utility/fim_dump.hpp"
+#include "utility/fim_profile.h"
 
 #define LENGTH (128 * 1024)
 
@@ -256,6 +257,76 @@ int fim_elt_add_4(bool block)
     return ret;
 }
 
+int fim_elt_add_profile(bool block)
+{
+    int ret = 0;
+
+    /* __FIM_API__ call : Initialize FimRuntime */
+    FimInitialize(RT_TYPE_HIP, FIM_FP16);
+
+    /* __FIM_API__ call : Create FIM Buffer Object */
+    FimBo* host_input0 = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_HOST);
+    FimBo* host_input1 = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_HOST);
+    FimBo* host_output = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_HOST);
+    FimBo* golden_output = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_HOST);
+
+    FimBo* fim_input1 = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_FIM);
+    FimBo* device_output = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_FIM);
+    FimBo* fim_input0 = FimCreateBo(LENGTH, 1, 1, 1, FIM_FP16, MEM_TYPE_FIM);
+
+    std::string test_vector_data = TEST_VECTORS_DATA;
+    test_vector_data.append("/test_vectors/");
+
+    std::string input0 = test_vector_data + "load/elt_add/input0_256KB.dat";
+    std::string input1 = test_vector_data + "load/elt_add/input1_256KB.dat";
+    std::string output = test_vector_data + "load/elt_add/output_256KB.dat";
+    std::string output_dump = test_vector_data + "dump/elt_add/output_256KB.dat";
+
+    load_data(input0.c_str(), (char*)host_input0->data, host_input0->size);
+    load_data(input1.c_str(), (char*)host_input1->data, host_input1->size);
+    load_data(output.c_str(), (char*)golden_output->data, golden_output->size);
+
+    /* __FIM_API__ call : Preload weight data on FIM memory */
+    FimCopyMemory(fim_input0, host_input0, HOST_TO_FIM);
+    FimCopyMemory(fim_input1, host_input1, HOST_TO_FIM);
+
+    FimExecuteDummy();
+/* __FIM_API__ call : Execute FIM kernel (ELT_ADD) */
+#ifdef TARGET
+    int iter;
+    FIM_PROFILE_TICK_A(ELT_ADD_1);
+    for (iter = 0; iter < 1000; iter++) {
+#endif
+        FimExecuteAdd(device_output, fim_input0, fim_input1, block);
+
+        if (!block) FimSynchronize();
+#ifdef TARGET
+    }
+    FIM_PROFILE_TOCK_A(ELT_ADD_1);
+    std::cout << "[ " << iter << " execution time ]" << std::endl;
+#endif
+
+    FimCopyMemory(host_output, device_output, FIM_TO_HOST);
+
+    ret = compare_data((char*)golden_output->data, (char*)host_output->data, host_output->size);
+
+    dump_data(output_dump.c_str(), (char*)host_output->data, host_output->size);
+
+    /* __FIM_API__ call : Free memory */
+    FimDestroyBo(host_input0);
+    FimDestroyBo(host_input1);
+    FimDestroyBo(host_output);
+    FimDestroyBo(golden_output);
+    FimDestroyBo(device_output);
+    FimDestroyBo(fim_input0);
+    FimDestroyBo(fim_input1);
+
+    /* __FIM_API__ call : Deinitialize FimRuntime */
+    FimDeinitialize();
+
+    return ret;
+}
+
 TEST(HIPIntegrationTest, FimEltAdd1Sync) { EXPECT_TRUE(fim_elt_add_1(true) == 0); }
 TEST(HIPIntegrationTest, FimEltAdd1Async) { EXPECT_TRUE(fim_elt_add_1(false) == 0); }
 TEST(HIPIntegrationTest, FimEltAdd2Sync) { EXPECT_TRUE(fim_elt_add_2(true) == 0); }
@@ -264,3 +335,4 @@ TEST(HIPIntegrationTest, FimEltAdd3Sync) { EXPECT_TRUE(fim_elt_add_3(true) == 0)
 TEST(HIPIntegrationTest, FimEltAdd3Async) { EXPECT_TRUE(fim_elt_add_3(false) == 0); }
 TEST(HIPIntegrationTest, FimEltAdd4Sync) { EXPECT_TRUE(fim_elt_add_4(true) == 0); }
 TEST(HIPIntegrationTest, FimEltAdd4ASync) { EXPECT_TRUE(fim_elt_add_4(false) == 0); }
+TEST(HIPIntegrationTest, FimEltAddProfileSync) { EXPECT_TRUE(fim_elt_add_profile(true) == 0); }
