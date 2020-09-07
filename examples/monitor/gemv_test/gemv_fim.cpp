563e411e401b005a113f3da89bd1b47f07b98f70
Hyeonsu Kim <hyeonsu.kim@samsung.com>
2020-09-07 06:50:00 +0000

    Update gemv test app for monitor
    - Sync up gemv monitor kernel with runtime kernel
    
    Signed-off-by: Hyeonsu Kim <hyeonsu.kim@samsung.com>

--- examples/monitor/gemv_test/gemv_fim.cpp
+++ examples/monitor/gemv_test/gemv_fim.cpp
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #include <iostream>
+#include "hip/hip_fp16.h"
 #include "hip/hip_runtime.h"
 
-extern "C" uint64_t fmm_map_fim(uint32_t, uint32_t, uint64_t);
-
-#define RADEON7 1
-
-#define FIM_RESERVED_8GB (0x200000000)
-#define FIM_RESERVED_16GB (0x400000000)
-#define CHANNEL (64)
-#define CH_BIT (6)
-
-#define PREPARE_KERNEL 1
 #define PARK_IN 1
 #define CHANGE_SB_HAB 1
 #define PROGRAM_CRF 1
-#define CHANGE_HAB_HABFIM 1
-#define PROGRAM_CRF 1
 #define COMPUTE_GEMV 1
-#define CHANGE_HABFIM_HAB 1
 #define CHANGE_HAB_SB 1
 #define PARK_OUT 1
+#define INTEGRAL_SUM 1
+
+extern "C" uint64_t fmm_map_fim(uint32_t, uint32_t, uint64_t);
 
 #define CHECK(cmd)                                                                                              \
     {                                                                                                           \
@@ -33,102 +24,26 @@ extern "C" uint64_t fmm_map_fim(uint32_t, uint32_t, uint64_t);
 
 __device__ inline void R_CMD(uint8_t* src)
 {
-    asm volatile("global_load_dwordx4 v[24:27], %0, off, glc, slc" ::"v"(src): "v24", "v25", "v26", "v27");
+    asm volatile("global_load_dwordx4 v[24:27], %0, off, glc, slc" ::"v"(src) : "v24", "v25", "v26", "v27");
 }
 
 __device__ inline void W_CMD(uint8_t* dst)
 {
-    asm volatile("global_store_dwordx4 %0, v[24:27], off, glc, slc" ::"v"(dst): "v24", "v25", "v26", "v27");
+    asm volatile("global_store_dwordx4 %0, v[24:27], off, glc, slc" ::"v"(dst) : "v24", "v25", "v26", "v27");
 }
 
-__device__ inline void W_CMD_R(uint8_t* dst, uint8_t* src)
-{
-    ((int4*)dst)[0] = ((int4*)src)[0];
-#if 0
-    switch (hipThreadIdx_x) {
-        case 0:
-            asm volatile("global_load_dwordx4 v[8:11], %0, off, glc, slc" ::"v"(src): "v8", "v9, "v10", "v11");
-            asm volatile("global_store_dwordx4 %0, v[8:11], off, glc, slc" ::"v"(dst));
-            break;
-        case 1:
-            asm volatile("global_load_dwordx4 v[12:15], %0, off, glc, slc" ::"v"(src): "v12", "v13, "v14", "v15");
-            asm volatile("global_store_dwordx4 %0, v[12:15], off, glc, slc" ::"v"(dst));
-            break;
-        case 2:
-            asm volatile("global_load_dwordx4 v[16:19], %0, off, glc, slc" ::"v"(src): "v16", "v17, "v18", "v19");
-            asm volatile("global_store_dwordx4 %0, v[16:19], off, glc, slc" ::"v"(dst));
-            break;
-        case 3:
-            asm volatile("global_load_dwordx4 v[20:23], %0, off, glc, slc" ::"v"(src): "v20", "v21, "v22", "v23");
-            asm volatile("global_store_dwordx4 %0, v[20:23], off, glc, slc" ::"v"(dst));
-            break;
-        case 4:
-            asm volatile("global_load_dwordx4 v[24:27], %0, off, glc, slc" ::"v"(src): "v24", "v25, "v26", "v27");
-            asm volatile("global_store_dwordx4 %0, v[24:27], off, glc, slc" ::"v"(dst));
-            break;
-        case 5:
-            asm volatile("global_load_dwordx4 v[28:31], %0, off, glc, slc" ::"v"(src): "v28", "v29, "v30", "v31");
-            asm volatile("global_store_dwordx4 %0, v[28:31], off, glc, slc" ::"v"(dst));
-            break;
-        case 6:
-            asm volatile("global_load_dwordx4 v[32:35], %0, off, glc, slc" ::"v"(src): "v32", "v33, "v34", "v35");
-            asm volatile("global_store_dwordx4 %0, v[32:35], off, glc, slc" ::"v"(dst));
-            break;
-        case 7:
-            asm volatile("global_load_dwordx4 v[36:39], %0, off, glc, slc" ::"v"(src): "v36", "v37, "v38", "v39");
-            asm volatile("global_store_dwordx4 %0, v[36:39], off, glc, slc" ::"v"(dst));
-            break;
-        case 8:
-            asm volatile("global_load_dwordx4 v[40:43], %0, off, glc, slc" ::"v"(src): "v40", "v41, "v42", "v43");
-            asm volatile("global_store_dwordx4 %0, v[40:43], off, glc, slc" ::"v"(dst));
-            break;
-        case 9:
-            asm volatile("global_load_dwordx4 v[44:47], %0, off, glc, slc" ::"v"(src): "v44", "v45, "v46", "v47");
-            asm volatile("global_store_dwordx4 %0, v[44:47], off, glc, slc" ::"v"(dst));
-            break;
-        case 10:
-            asm volatile("global_load_dwordx4 v[48:51], %0, off, glc, slc" ::"v"(src): "v48", "v49, "v50", "v51");
-            asm volatile("global_store_dwordx4 %0, v[48:51], off, glc, slc" ::"v"(dst));
-            break;
-        case 11:
-            asm volatile("global_load_dwordx4 v[52:55], %0, off, glc, slc" ::"v"(src): "v52", "v53, "v54", "v55");
-            asm volatile("global_store_dwordx4 %0, v[52:55], off, glc, slc" ::"v"(dst));
-            break;
-        case 12:
-            asm volatile("global_load_dwordx4 v[56:59], %0, off, glc, slc" ::"v"(src): "v56", "v57, "v58", "v59");
-            asm volatile("global_store_dwordx4 %0, v[56:59], off, glc, slc" ::"v"(dst));
-            break;
-        case 13:
-            asm volatile("global_load_dwordx4 v[60:63], %0, off, glc, slc" ::"v"(src): "v60", "v61, "v62", "v63");
-            asm volatile("global_store_dwordx4 %0, v[60:63], off, glc, slc" ::"v"(dst));
-            break;
-        case 14:
-            asm volatile("global_load_dwordx4 v[64:67], %0, off, glc, slc" ::"v"(src): "v64", "v65, "v66", "v67");
-            asm volatile("global_store_dwordx4 %0, v[64:67], off, glc, slc" ::"v"(dst));
-            break;
-        case 15:
-            asm volatile("global_load_dwordx4 v[68:71], %0, off, glc, slc" ::"v"(src)): "v68", "v69, "v70", "v71");
-            asm volatile("global_store_dwordx4 %0, v[68:71], off, glc, slc" ::"v"(dst));
-            break;
-        default:
-            break;
-    }
-#endif
-}
+__device__ inline void W_CMD_R(uint8_t* dst, uint8_t* src) { ((int4*)dst)[0] = ((int4*)src)[0]; }
 
-
-__device__ inline void W_CMD_R_2TH(uint8_t* dst, uint8_t* src)
+__device__ inline void B_CMD(int type)
 {
-    if (hipThreadIdx_x == 0) {
-        asm volatile("global_load_dwordx4 v[20:23], %0, off, glc, slc" ::"v"(src));
-        asm volatile("global_store_dwordx4 %0, v[20:23], off, glc, slc" ::"v"(dst));
+    if (type == 0) {
+        __syncthreads();
+        asm volatile("s_waitcnt vmcnt(0) lgkmcnt(0)");
     } else {
-        asm volatile("global_load_dwordx4 v[24:27], %0, off, glc, slc" ::"v"(src));
-        asm volatile("global_store_dwordx4 %0, v[24:27], off, glc, slc" ::"v"(dst));
+        __threadfence();
+        asm volatile("s_waitcnt vmcnt(0) lgkmcnt(0)");
     }
 }
-
-__device__ inline void B_CMD(int type) { (type == 0) ? __syncthreads() : __threadfence(); }
 
 __device__ inline unsigned int mask_by_bit(unsigned int value, int start, int end)
 {
@@ -137,9 +52,6 @@ __device__ inline unsigned int mask_by_bit(unsigned int value, int start, int en
     return value & ((1 << length) - 1);
 }
 
-// 64CH, 32GB address map
-// rank 1b | row 14b | col(msb) 3b | ba(msb) 1b | bg 2b | ba(lsb) 1b | chan(msb) 5b | col(lsb) 1b | chan[0] 1b |
-// col(lsb) 1b | col(bst) 5b
 __device__ uint64_t addr_gen(unsigned int ch, unsigned int rank, unsigned int bg, unsigned int ba, unsigned int row,
                              unsigned int col)
 {
@@ -148,7 +60,7 @@ __device__ uint64_t addr_gen(unsigned int ch, unsigned int rank, unsigned int bg
     int num_bank_high_bit_ = 1;
     int num_bankgroup_bit_ = 2;
     int num_bank_low_bit_ = 1;
-    int num_chan_bit_ = CH_BIT;
+    int num_chan_bit_ = 6;
     int num_col_low_bit_ = 1;
     int num_offset_bit_ = 5;
 
@@ -183,18 +95,28 @@ __device__ uint64_t addr_gen(unsigned int ch, unsigned int rank, unsigned int bg
 
     addr <<= num_offset_bit_;
 
-#ifdef RADEON7
-    uint64_t mask = 0x1FFFFFFFF;
-    addr &= mask;
-#endif
     return addr;
 }
 
-__global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gemv_tmp_buffer, uint8_t* fim_input, int batch_dim,
-                         int n_in_tile, int n_out_tile, uint8_t* crf_binary, int crf_size, uint8_t* hab_to_fim,
-                         uint8_t* fim_to_hab)
+uint8_t gemv_crf[32] = {
+    0x00, 0x80, 0x10, 0x3b, 0x02, 0x38, 0x00, 0xe0, 0x00, 0x80, 0x18, 0x3b, 0x02, 0x38, 0x00, 0xe0,
+    0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+uint8_t gemv_hab_to_hab_fim[32] = {
+    0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+uint8_t gemv_hab_fim_to_hab[32] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+__global__ void gemv_fim_64cu_64th_fp16(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gemv_tmp_buffer,
+                                        uint8_t* fim_input, uint8_t* output, int batch_dim, int n_in_tile,
+                                        int n_out_tile, int output_dim)
 {
-#if PREPARE_KERNEL
     int num_col = 32;
     int num_bg = 4;
     int num_ba = 4;
@@ -205,16 +127,15 @@ __global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gem
     int num_in_tile = n_in_tile;
     int num_out_tile = n_out_tile;
 
-    int gidx = hipThreadIdx_x / 2;
-    uint64_t offset = (hipThreadIdx_x % 2) * 0x10;
+    int gidx = hipThreadIdx_x >> 1;
+    uint64_t offset = (hipThreadIdx_x % 2) << 4;
     uint64_t addr;
-#endif
-    /* Radeon7(VEGA20) memory is 16GB but our target is 32GB system */
-    /* so program_crf and chagne_fim_mode functions can not access to over 8GB in our system */
+
 #if PARK_IN
-    if (hipThreadIdx_x < num_bg * num_ba * 2) {
-        addr = addr_gen(hipBlockIdx_x, 0, gidx / num_ba, gidx % num_ba, 0, 0);
-        R_CMD(&fim_weight[addr + offset]);
+    /* num_bg * num_ba * 2 */
+    if (hipThreadIdx_x < 32) {
+        addr = addr_gen(hipBlockIdx_x, 0, gidx >> 2, gidx % num_ba, 0, 0);
+        R_CMD(&fim_ctr[addr + offset]);
     }
     B_CMD(0);
 #endif
@@ -238,31 +159,31 @@ __global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gem
 #endif
 
 #if PROGRAM_CRF
-    if (hipThreadIdx_x < 2 * crf_size) {
+    if (hipThreadIdx_x < 2 /* sizeof(gemv_crf) >> 4 */) {
         addr = addr_gen(hipBlockIdx_x, 0, 0, 1, 0x3fff, 0x4 + gidx);
-        W_CMD_R(&fim_ctr[addr + offset], crf_binary + hipThreadIdx_x * 16);
+        W_CMD_R(&fim_ctr[addr + offset], gemv_crf + (hipThreadIdx_x << 4));
     }
     B_CMD(0);
 #endif
 
 #if COMPUTE_GEMV
-    if (hipThreadIdx_x < 2 * num_grf) {
+    if (hipThreadIdx_x < 16 /* 2 * num_grf */) {
         for (int b_idx = 0; b_idx < batch_dim; b_idx++) {
             for (int o_idx = 0; o_idx < num_out_tile; o_idx++) {
                 addr = addr_gen(hipBlockIdx_x, 0, 0, 0, 0x3fff, 0x0);
-                W_CMD_R(&fim_ctr[addr + offset], hab_to_fim + (hipThreadIdx_x % 2) * 16);
+                W_CMD_R(&fim_ctr[addr + offset], gemv_hab_to_hab_fim + ((hipThreadIdx_x % 2) << 4));
                 B_CMD(1);
 
-                uint64_t i_offset = b_idx * num_in_tile * num_grf * trans_size;
-                int r_offset = o_idx * num_in_tile / 2;
+                uint64_t i_offset = (b_idx * num_in_tile << 3) + gidx;
+                int r_offset = (o_idx * num_in_tile) >> 1;
 
                 for (int i_idx = 0; i_idx < num_in_tile; i_idx += 2) {
-                    uint64_t i_addr = i_offset + (i_idx * num_grf * trans_size) + hipThreadIdx_x * 16;
+                    uint64_t i_addr = (i_offset + (i_idx << 3)) << 5;
                     addr = addr_gen(hipBlockIdx_x, 0, 0, 1, 0x3fff, 0x8 + gidx);
-                    W_CMD_R(&fim_ctr[addr + offset], &fim_input[i_addr]);
+                    W_CMD_R(&fim_ctr[addr + offset], &fim_input[i_addr + offset]);
                     B_CMD(1);
 
-                    row = (i_idx / 2 + r_offset) * 2;
+                    row = ((i_idx >> 1) + r_offset) << 1;
                     col = gidx;
 
                     addr = addr_gen(hipBlockIdx_x, 0, 0, 0, row, col);
@@ -292,12 +213,12 @@ __global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gem
                 }
 
                 for (int i_idx = 1; i_idx < num_in_tile; i_idx += 2) {
-                    uint64_t i_addr = i_offset + (i_idx * num_grf * trans_size) + hipThreadIdx_x * 16;
+                    uint64_t i_addr = (i_offset + (i_idx << 3)) << 5;
                     addr = addr_gen(hipBlockIdx_x, 0, 0, 1, 0x3fff, 0x8 + gidx);
-                    W_CMD_R(&fim_ctr[addr + offset], &fim_input[i_addr]);
+                    W_CMD_R(&fim_ctr[addr + offset], &fim_input[i_addr + offset]);
                     B_CMD(1);
 
-                    row = (i_idx / 2 + r_offset) * 2;
+                    row = ((i_idx >> 1) + r_offset) << 1;
                     col = gidx;
 
                     addr = addr_gen(hipBlockIdx_x, 0, 0, 1, row, col);
@@ -326,7 +247,7 @@ __global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gem
                     B_CMD(1);
                 }
                 loc = b_idx * num_out_tile * num_grf + o_idx * num_grf + gidx;
-                row = loc / num_col;
+                row = loc >> 5;  // loc / num_col
                 col = loc % num_col;
 
                 addr = addr_gen(hipBlockIdx_x, 0, 0, 1, row, col);
@@ -334,12 +255,12 @@ __global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gem
                 B_CMD(1);
 
                 addr = addr_gen(hipBlockIdx_x, 0, 0, 0, 0x3fff, 0x0);
-                W_CMD_R(&fim_ctr[addr + offset], fim_to_hab + (hipThreadIdx_x % 2) * 16);
+                W_CMD_R(&fim_ctr[addr + offset], gemv_hab_fim_to_hab + ((hipThreadIdx_x % 2) << 4));
                 B_CMD(1);
             }
         }
     }
-	B_CMD(0);
+    B_CMD(0);
 #endif
 
 #if CHANGE_HAB_SB
@@ -351,32 +272,49 @@ __global__ void gemv_fim(uint8_t* fim_ctr, uint8_t* fim_weight, uint8_t* fim_gem
 #endif
 
 #if PARK_OUT
-    if (hipThreadIdx_x < num_bg * num_ba * 2) {
+    if (hipThreadIdx_x < 32) {
         addr = addr_gen(hipBlockIdx_x, 0, gidx / num_ba, gidx % num_ba, 0, 0);
         R_CMD(&fim_weight[addr + offset]);
     }
     B_CMD(0);
+#endif
+
+#if INTEGRAL_SUM
+    int bg = hipThreadIdx_x >> 4;
+    int ba = (((hipThreadIdx_x >> 3) % 2) << 1) + 1;
+    int out_idx = (hipBlockIdx_x << 6) + hipThreadIdx_x;
+    half t_output;
+    for (int i = 0; i < batch_dim * num_out_tile; i++) {
+        if (out_idx < output_dim) {
+            t_output = 0;
+            row = i >> 2;
+            col = hipThreadIdx_x % 8 + ((i % 4) << 3);
+            addr = addr_gen(hipBlockIdx_x, 0, bg, ba, row, col);
+            for (int j = 0; j < 16; j++) {
+                t_output += fim_gemv_tmp_buffer[addr + j];
+            }
+            ((half*)output)[out_idx] = ((half*)output)[out_idx] + t_output;
+        }
+    }
 #endif
 }
 
 int main(int argc, char* argv[])
 {
     uint64_t fim_base;
-    uint64_t *mode1_d, *mode2_d, *crf_bin_d;
-    uint64_t *mode1_h, *mode2_h, *crf_bin_h;
     uint16_t* input;
+    uint16_t* output;
     size_t N = 4;
     size_t Nbytes = N * sizeof(uint64_t);
     static int device = 0;
 
     int num_fim_blocks = 8;
-    int num_fim_chan = CHANNEL;
+    int num_fim_chan = 64;
     int num_grf = 8;
     int trans_size = 32;
 
-    int in_size = 1024;
+    int in_size = 512;
     int out_size = 4096;
-    int row_offset = 0x100000;
 
     CHECK(hipSetDevice(device));
     hipDeviceProp_t props;
@@ -399,39 +337,13 @@ int main(int argc, char* argv[])
       ARG2 : gpu-id
       ARG3 : block size
     ********************************************/
-    uint64_t bsize = FIM_RESERVED_8GB;
+    //uint64_t bsize = 8589934592;  // 8 * 1024 * 1024 * 1024;
+    uint64_t bsize = 17179869184;  // 16 * 1024 * 1024 * 1024;
     fim_base = fmm_map_fim(1, gpu_id, bsize);
     std::cout << std::hex << "fimBaseAddr = " << fim_base << std::endl;
 
-    crf_bin_h = (uint64_t*)malloc(Nbytes);
-    CHECK(crf_bin_h == 0 ? hipErrorOutOfMemory : hipSuccess);
-    mode1_h = (uint64_t*)malloc(Nbytes);
-    CHECK(mode1_h == 0 ? hipErrorOutOfMemory : hipSuccess);
-    mode2_h = (uint64_t*)malloc(Nbytes);
-    CHECK(mode2_h == 0 ? hipErrorOutOfMemory : hipSuccess);
-
-    crf_bin_h[0] = 0x0080103b023800e0;
-    crf_bin_h[1] = 0x0080183b023800e0;
-    crf_bin_h[2] = 0x07000000000000f0;
-    crf_bin_h[3] = 0x0000000000000000;
-    mode1_h[0] = 0x0000000000000001;
-    mode1_h[1] = 0x0000000000000000;
-    mode1_h[2] = 0x0000000000000000;
-    mode1_h[3] = 0x0000000000000000;
-    mode2_h[0] = 0x0000000000000000;
-    mode2_h[1] = 0x0000000000000000;
-    mode2_h[2] = 0x0000000000000000;
-    mode2_h[3] = 0x0000000000000000;
-
-    CHECK(hipMalloc(&crf_bin_d, Nbytes));
-    CHECK(hipMalloc(&mode1_d, Nbytes));
-    CHECK(hipMalloc(&mode2_d, Nbytes));
-
-    CHECK(hipMemcpy(crf_bin_d, crf_bin_h, Nbytes, hipMemcpyHostToDevice));
-    CHECK(hipMemcpy(mode1_d, mode1_h, Nbytes, hipMemcpyHostToDevice));
-    CHECK(hipMemcpy(mode2_d, mode2_h, Nbytes, hipMemcpyHostToDevice));
-
     CHECK(hipMalloc(&input, in_size * sizeof(uint16_t)));
+    CHECK(hipMalloc(&output, out_size * sizeof(uint16_t)));
 
     int n_in_tile = in_size * sizeof(uint16_t) / trans_size / num_grf;
     int n_out_tile = out_size / (num_fim_chan * num_fim_blocks * num_grf);
@@ -439,16 +351,12 @@ int main(int argc, char* argv[])
     const unsigned blocks = 64;
     const unsigned threadsPerBlock = 64;
 
-    hipLaunchKernelGGL(gemv_fim, dim3(blocks), dim3(threadsPerBlock), 0, 0, (uint8_t*)fim_base,
-                       (uint8_t*)fim_base + 0x100000, (uint8_t*)fim_base + 0x200000, (uint8_t*)input, 1, n_in_tile,
-                       n_out_tile, (uint8_t*)crf_bin_d, 1, (uint8_t*)mode1_d, (uint8_t*)mode2_d);
+    hipLaunchKernelGGL(gemv_fim_64cu_64th_fp16, dim3(blocks), dim3(threadsPerBlock), 0, 0,
+                       (uint8_t*)fim_base /* ctr  base */, (uint8_t*)fim_base + 0x100000 /* weight */,
+                       (uint8_t*)fim_base + 0x200000, /* fim hw output */
+                       (uint8_t*)input, (uint8_t*)output, 1, n_in_tile, n_out_tile, out_size);
+    hipStreamSynchronize(NULL);
 
-    free(mode1_h);
-    free(mode2_h);
-    free(crf_bin_h);
-
-    hipFree(mode1_d);
-    hipFree(mode2_d);
-    hipFree(crf_bin_d);
     hipFree(input);
+    hipFree(output);
 }
