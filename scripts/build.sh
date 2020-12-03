01224d014c581a4637f9140add2ecb9faba0b11d
Sudhakar M K <sudhakar.mk@samsung.com>
2020-12-03 09:27:31 +0000

    Separate emulator and target modes
    This commit separates emulator and target modes
    
    Signed-off-by: Sudhakar M K <sudhakar.mk@samsung.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,8 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-OPTIONS=do:mt:v
-LONGOPTS=debug,output:,miopen,target:,verbose
+OPTIONS=do:mt:ve
+LONGOPTS=debug,output:,miopen,target:,verbose,emulator
 
 # -regarding ! and PIPESTATUS see above
 # -temporarily store output to be able to check for errors
@@ -27,7 +27,7 @@ fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
-d=n v=n proj_cmake_dir=- miopen=n target_device="emulator"
+d=n v=n proj_cmake_dir=- miopen=n target_device="mi50" emulator=n
 
 # now enjoy the options in order and nicely split until we see --
 while true; do
@@ -53,6 +53,10 @@ while true; do
             target_device="$2"
             shift 2
             ;;
+	-e|--emulator)
+	    emulator=y
+	    shift
+	    ;;
         --)
             shift
             break
@@ -86,12 +90,16 @@ else
     cmake_build_options="${cmake_build_options} -DMIOPEN_APPS=OFF"
 fi
 
-if [ $target_device = "emulator" ]; then
-    cmake_build_options="${cmake_build_options} -DTARGET=OFF -DMI50=OFF -DRADEON7=OFF"
-elif [ $target_device = "mi50" ]; then
-    cmake_build_options="${cmake_build_options} -DTARGET=ON -DMI50=ON"
+if [ $emulator = "y" ]; then
+    cmake_build_options="${cmake_build_options} -DTARGET=OFF -DEMULATOR=ON"
+else
+    cmake_build_options="${cmake_build_options} -DTARGET=ON -DEMULATOR=OFF"
+fi
+
+if [ $target_device = "mi50" ]; then
+    cmake_build_options="${cmake_build_options} -DMI50=ON"
 elif [ $target_device = "radeon7" ]; then
-    cmake_build_options="${cmake_build_options} -DTARGET=ON -DRADEON7=ON"
+    cmake_build_options="${cmake_build_options} -DRADEON7=ON"
 fi
 
 echo "${cmake_build_options}"
