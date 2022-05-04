59140444000a66909bb0d427f9847e4f4b10c22e
Hyeonsu Kim <hyeonsu.kim@samsung.com>
2022-05-04 07:07:42 +0000

    Update miopen benchmark script
    - enable functional verification flag
    - remove transpose matrix for gpu only benchmark
    
    Signed-off-by: Hyeonsu Kim <hyeonsu.kim@samsung.com>

--- scripts/run_miopen_gemv_perf.sh
+++ scripts/run_miopen_gemv_perf.sh
@@ -9,20 +9,20 @@ echo "[GPU+HBM mode test]"
 echo ""
 export ENABLE_PIM=0
 export ENABLE_NEXT_PIM=0
-MIOpenDriver gemmfp16 -F 1 -m 1 -k 1024 -n 4096 -v 1 -V 0 -i 100
+MIOpenDriver gemmfp16 -F 1 -m 1 -k 1024 -n 4096 -v 0 -V 1 -i 100
 echo ""
 echo ""
 echo "[GPU+PIM mode test]"
 echo ""
 export ENABLE_PIM=1
 export ENABLE_NEXT_PIM=0
-MIOpenDriver gemmfp16 -F 1 -m 1 -k 1024 -n 4096 -v 1 -V 0 -i 100
+MIOpenDriver gemmfp16 -F 1 -m 1 -k 1024 -n 4096 -v 1 -V 1 -i 100
 echo ""
 echo ""
 echo "[Next GPU+PIM mode test]"
 echo ""
 export ENABLE_PIM=1
 export ENABLE_NEXT_PIM=1
-MIOpenDriver gemmfp16 -F 1 -m 1 -k 1024 -n 4096 -v 1 -V 0 -i 100
+MIOpenDriver gemmfp16 -F 1 -m 1 -k 1024 -n 4096 -v 1 -V 1 -i 100
 echo ""
 echo ""
