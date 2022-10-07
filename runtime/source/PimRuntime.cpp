6581ed9b39484c51c8fb7a957871cfba33b15eb4
rashmi.lathi <rashmi.lathi@samsung.com>
2022-10-07 05:55:25 +0000

    Copy CRF binary from Host to Device

--- runtime/source/PimRuntime.cpp
+++ runtime/source/PimRuntime.cpp
@@ -476,17 +476,17 @@ PimBo* PimRuntime::execute_program(PimCompiledObj* obj, PimTarget* target, std::
     DLOG(INFO) << "[START] " << __FUNCTION__ << " called";
     pimc_driver::PimCDriver pimc_driver;
     auto kernel = pimc_driver.compile_code(obj->kernel, obj->crf_binary);
-    //struct kArgs {
     uint8_t* args[obj->op_order.size() + 1];// +1 for gpim_base_addr
-    //};
-    //kArgs *kernel_args = new kArgs;
+    uint8_t *crf_binary_device;
+    hipMalloc((void **)&crf_binary_device, 128);
+    hipMemcpy((void *)crf_binary_device, (uint8_t *)(obj->crf_binary.c_str()), obj->crf_binary.size(), hipMemcpyHostToDevice);
     for (size_t i = 0; i < obj->op_order.size(); i++) {
         if (obj->pimbo_map.find(obj->op_order[i]) != obj->pimbo_map.end())
             args[i] = static_cast<uint8_t*>(obj->pimbo_map[obj->op_order[i]]->data);
         else if (obj->op_order[i] == "crf_binary") {
             //Push pim_ctr
             args[i++] = (uint8_t *)g_pim_base_addr[0];
-            args[i] = (uint8_t *)&obj->crf_binary;
+            args[i] = crf_binary_device;
         }
         else {
             DLOG(ERROR) << "PimBo not found in map";
@@ -494,12 +494,7 @@ PimBo* PimRuntime::execute_program(PimCompiledObj* obj, PimTarget* target, std::
             return nullptr;
         }
     }
-    //size_t size = sizeof(*kernel_args);
     size_t size = obj->op_order.size() + 1;
-    //std::cout<<"Printing kernel args: \n";
-    //for (int i = 0; i < size; i++)
-    //    std::cout<< i <<" = " <<args[i] <<" ,  ";;
-    //std::cout << std::endl;
     void *config[5] = {HIP_LAUNCH_PARAM_BUFFER_POINTER, nullptr, HIP_LAUNCH_PARAM_BUFFER_SIZE, &size, HIP_LAUNCH_PARAM_END};
     config[1] = static_cast<void*>(args);
     hipModuleLaunchKernel(kernel, obj->num_blocks, 1, 1, obj->num_threads, 1, 1, 0, nullptr, NULL, reinterpret_cast<void **>(&config));
