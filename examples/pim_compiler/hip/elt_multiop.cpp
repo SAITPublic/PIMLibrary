9a4c4f3bac5f290208b84bb170cc230b9d85edb0
rashmi.lathi <rashmi.lathi@samsung.com>
2022-10-14 05:38:20 +0000

    Add element-wise multi-operation testcase
    Signed-off-by: rashmi.lathi <rashmi.lathi@samsung.com>

--- examples/pim_compiler/hip/elt_multiop.cpp
+++ examples/pim_compiler/hip/elt_multiop.cpp
@@ -10,7 +10,7 @@ using namespace pimc;
 using namespace pimc::frontend;
 using half_float::half;
 
-int pimc_eltwise_add(int h, int w){
+int pimc_elt_multiop(int h, int w){
     int ret = 0;
     PimInitialize(RT_TYPE_HIP, PIM_FP16);
     PimDesc* pim_desc = PimCreateDesc(1, 1, h, w, PIM_FP16);
@@ -18,14 +18,17 @@ int pimc_eltwise_add(int h, int w){
     // __PIM_API__ call : Create PIM Buffer Object
     PimBo* host_input0 = PimCreateBo(pim_desc, MEM_TYPE_HOST);
     PimBo* host_input1 = PimCreateBo(pim_desc, MEM_TYPE_HOST);
+    PimBo* host_input2 = PimCreateBo(pim_desc, MEM_TYPE_HOST);
     PimBo* host_output = PimCreateBo(pim_desc, MEM_TYPE_HOST);
     PimBo* golden_output = PimCreateBo(pim_desc, MEM_TYPE_HOST);
     PimBo* pim_input0 = PimCreateBo(pim_desc, MEM_TYPE_PIM);
     PimBo* pim_input1 = PimCreateBo(pim_desc, MEM_TYPE_PIM);
+    PimBo* pim_input2 = PimCreateBo(pim_desc, MEM_TYPE_PIM);
 
     // Load operand data
     set_rand_half_data((half_float::half*)host_input0->data, (half_float::half)0.5, (h * w));
     set_rand_half_data((half_float::half*)host_input1->data, (half_float::half)0.5, (h * w));
+    set_rand_half_data((half_float::half*)host_input2->data, (half_float::half)0.5, (h * w));
 
     //for (int i = 0; i < (h * w); i++) {
     //    ((half_float::half*)host_input0->data)[i] = 4.0;
@@ -33,25 +36,28 @@ int pimc_eltwise_add(int h, int w){
     //}
 
     //Compute Golden output
-    addCPU((half_float::half*)host_input0->data, (half_float::half*)host_input1->data, (half_float::half*)golden_output->data, (h * w));
+    for (int i = 0; i < h * w; i++)
+            ((half_float::half*)golden_output->data)[i] = ((half_float::half*)host_input0->data)[i] * ((half_float::half*)host_input1->data)[i] + ((half_float::half*)host_input2->data)[i];
 
     //Copy input data from HOST to PIM
     PimCopyMemory(pim_input0, host_input0, HOST_TO_PIM);
     PimCopyMemory(pim_input1, host_input1, HOST_TO_PIM);
+    PimCopyMemory(pim_input2, host_input2, HOST_TO_PIM);
 
     //Declare variables
     IndexVar i(0, h, "i");
     IndexVar j(0, w, "j");
+    Buffer A(h, w, "A");
     Buffer B(h, w, "B");
     Buffer C(h, w, "C");
     Var D("D");
 
     //Define Computation
-    D[i][j] = C[i][j] + B[i][j];
+    D[i][j] = A[i][j] * B[i][j] + C[i][j];
 
     //Run
     PimTarget* target = PimCreateTarget(RT_TYPE_HIP, PIM_FP16, GPU);
-    PimCompiledObj* obj = PimBuildProgram(D, {B, C}, {pim_input0, pim_input1}, target);
+    PimCompiledObj* obj = PimBuildProgram(D, {A, B, C}, {pim_input0, pim_input1, pim_input2}, target);
     PimBo* device_output = PimExecuteProgram(obj, target);
     PimCopyMemory(host_output, device_output, PIM_TO_HOST);
     ret = compare_half_relative((half*)host_output->data, (half*)golden_output->data, (h*w));
@@ -62,11 +68,13 @@ int pimc_eltwise_add(int h, int w){
 
     PimDestroyBo(host_input0);
     PimDestroyBo(host_input1);
+    PimDestroyBo(host_input2);
     PimDestroyBo(host_output);
     PimDestroyBo(golden_output);
     PimDestroyBo(device_output);
     PimDestroyBo(pim_input0);
     PimDestroyBo(pim_input1);
+    PimDestroyBo(pim_input2);
     PimDestroyDesc(pim_desc);
 
     PimDeinitialize();
@@ -74,4 +82,4 @@ int pimc_eltwise_add(int h, int w){
 }
 
 
-TEST(PimCompilerIntegrationTestElt, EltAdd) { EXPECT_TRUE(pimc_eltwise_add(2, 4096) == 0); }
+TEST(PimCompilerIntegrationTestEltMultiop, EltMultiop) { EXPECT_TRUE(pimc_elt_multiop(2, 1024) == 0); }
