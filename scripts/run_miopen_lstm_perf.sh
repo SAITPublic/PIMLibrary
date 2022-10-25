ed2fd3a6b3843debb00016cce3eb308307c78063
Hyeonsu Kim <hyeonsu.kim@samsung.com>
2022-10-25 05:53:28 +0000

    Fix miopen test scripts
    - add cpu verify option
    - remove wrong transpose option
    
    Signed-off-by: Hyeonsu Kim <hyeonsu.kim@samsung.com>

--- scripts/run_miopen_lstm_perf.sh
+++ scripts/run_miopen_lstm_perf.sh
@@ -9,17 +9,17 @@ echo "[GPU+HBM mode test]"
 echo ""
 export ENABLE_PIM=0
 export ENABLE_NEXT_PIM=0
-MIOpenDriver rnnfp16 -F 1 -H 1024 -V 0 -W 2592 -c 1 -k 50 -l 5 -m lstm -n 1 -r 1 -w 1 -i 2
+MIOpenDriver rnnfp16 -F 1 -H 1024 -V 1 -W 2592 -c 1 -k 50 -l 5 -m lstm -n 1 -r 1 -w 1 -i 2
 
 echo ""
 echo "[GPU+PIM mode test]"
 echo ""
 export ENABLE_PIM=1
 export ENABLE_NEXT_PIM=0
-MIOpenDriver rnnfp16 -F 1 -H 1024 -V 0 -W 2592 -c 1 -k 50 -l 5 -m lstm -n 1 -r 1 -w 1 -i 2
+MIOpenDriver rnnfp16 -F 1 -H 1024 -V 1 -W 2592 -c 1 -k 50 -l 5 -m lstm -n 1 -r 1 -w 1 -i 2
 echo ""
 echo "[Next GPU+PIM mode test]"
 echo ""
 export ENABLE_PIM=1
 export ENABLE_NEXT_PIM=1
-MIOpenDriver rnnfp16 -F 1 -H 1024 -V 0 -W 2592 -c 1 -k 50 -l 5 -m lstm -n 1 -r 1 -w 1 -i 2
+MIOpenDriver rnnfp16 -F 1 -H 1024 -V 1 -W 2592 -c 1 -k 50 -l 5 -m lstm -n 1 -r 1 -w 1 -i 2
