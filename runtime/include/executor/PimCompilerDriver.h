410a5d837b05a831fab23aa74262d4285af394e9
Arnab Mitra <arnab.mitra@samsung.com>
2022-12-01 18:45:25 +0000

    Fix license and add documentation
    Signed-off-by: Arnab Mitra <arnab.mitra@samsung.com>

--- runtime/include/executor/PimCompilerDriver.h
+++ runtime/include/executor/PimCompilerDriver.h
@@ -3,13 +3,14 @@
  * @brief Header file for PimCompilerDriver base implementation
  *
  * Copyright (C) 2021 Samsung Electronics Co. LTD
- *
  * This software is a property of Samsung Electronics.
- * No part of this software, either material or conceptual may be copied or distributed, transmitted,
- * transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+ * No part of this software, either material or conceptual may be copied or
+ * distributed, transmitted, transcribed, stored in a retrieval system, or
+ * translated into any human or computer language in any form by any means,
  * electronic, mechanical, manual or otherwise, or disclosed
  * to third parties without the express written permission of Samsung Electronics.
- * (Use of the Software is restricted to non-commercial, personal or academic, research purpose only)
+ * (Use of the Software is restricted to non-commercial, personal or academic,
+ * research purpose only)
  */
 #ifndef __PIMC_DRIVER_HPP__
 #define __PIMC_DRIVER_HPP__
@@ -31,16 +32,25 @@ namespace runtime
 {
 namespace pimc_driver
 {
+/**
+ * Base class for defining host Compiler invoker
+ *
+ */
 class Compiler
 {
    public:
     /**
-     * @brief Constructor for HIPCompiler class
-     *
-     * @param pim_op pim_op object returned from compiler along with buffers from PIMLibrary
+     * @brief Constructor for Compiler class
+     * @param None
      */
     Compiler() = default;
     ~Compiler() {}
+    /**
+     * @brief Pure virtual function for invoking host compiler
+     * @param host_code host code in string format
+     * @param crf_binary generated CRF binary in string format
+     * @return None
+     */
     virtual void execute(std::string host_code, std::string crf_binary) = 0;
 
    private:
@@ -49,17 +59,49 @@ class Compiler
     Compiler &operator=(Compiler &&) = delete;
     Compiler &operator=(const Compiler &) = delete;
 };
-
+/**
+ * Base class for PIM Compiler and device execution
+ *
+ */
 class Driver
 {
    public:
+    /**
+     * @brief Constructor for Driver class
+     * @param None
+     */
     Driver() = default;
 
 #if PIM_COMPILER_ENABLE == 1
+    /**
+     * @brief Static Factory method for creating different drivers
+     * based on runtime information
+     * @param target PimTarget pointer containing runtime information
+     * @return std::shared_ptr<Driver> pointer to created driver
+     */
     static std::shared_ptr<Driver> create_driver(PimTarget *target);
+
+    /**
+     * @brief Pure virtual function for invoking PIMCompiler and generate
+     * binary and host code
+     * @param output Var designated as output
+     * @param inputs vector of input buffers
+     * @param input_pimbo vector of PIMLibrary input buffers
+     * @param target PimTarget pointer containing target information
+     * @param compile_opts compilation options
+     * @return PimCompiledObj* pointer to generated PIMCompiler object
+     */
     virtual PimCompiledObj *build_program(pimc::frontend::Var output, std::vector<pimc::frontend::Buffer> inputs,
                                           std::vector<PimBo *> input_pimbo, PimTarget *target,
                                           std::string compile_opts) = 0;
+    /**
+     * @brief Pure virtual function for invoking host compiler and
+     * executing generated code on target
+     * @param obj generated PimCompilerObj
+     * @param target PimTarget pointer containing target information
+     * @param launch_opts execution options
+     * @return PimBo* Output Pimbo
+     */
     virtual PimBo *execute_program(PimCompiledObj *obj, PimTarget *target, std::string launch_opts) = 0;
 #endif
     // todo:: Pass HW information from user
--- runtime/include/executor/hip/PimCompilerHipDriver.hpp
+++ runtime/include/executor/hip/PimCompilerHipDriver.hpp
@@ -1,17 +1,16 @@
 /**
- * @file PimCompilerHipDriver.h
+ * @file PimCompilerHipDriver.hpp
  * @brief Header file for PimCompilerDriver HIP implementation
  *
  * Copyright (C) 2021 Samsung Electronics Co. LTD
- *
  * This software is a property of Samsung Electronics.
  * No part of this software, either material or conceptual may be copied or
- * distributed, transmitted,
- * transcribed, stored in a retrieval system or translated into any human or
- * computer language in any form by any means,
+ * distributed, transmitted, transcribed, stored in a retrieval system, or
+ * translated into any human or computer language in any form by any means,
  * electronic, mechanical, manual or otherwise, or disclosed
- * to third parties without the express written permission of Samsung
- * Electronics.
+ * to third parties without the express written permission of Samsung Electronics.
+ * (Use of the Software is restricted to non-commercial, personal or academic,
+ * research purpose only)
  */
 #ifndef __PIMC_DRIVER_HIP_HPP__
 #define __PIMC_DRIVER_HIP_HPP__
@@ -33,17 +32,31 @@ namespace runtime
 {
 namespace pimc_driver
 {
+/**
+ * class for defining host HIP Compiler invoker
+ */
 class HIPCompiler : public Compiler
 {
    public:
     /**
      * @brief Constructor for HIPCompiler class
-     *
-     * @param pim_op pim_op object returned from compiler along with buffers from PIMLibrary
+     * @param None
      */
     HIPCompiler() : Compiler() {}
     ~HIPCompiler() {}
+    /**
+     * @brief Function for invoking host HIP compiler
+     * @param host_code host code in string format
+     * @param crf_binary generated CRF binary in string format
+     * @return None
+     */
     void execute(std::string hip_kernel, std::string crf_binary);
+
+    /**
+     * @brief Function to get generated kernel function
+     * @param None
+     * @return hipFunction_t generated HIP Function to be passed to HIP launch kernel
+     */
     hipFunction_t get_kernel_function() { return kernel_; }
    private:
     HIPCompiler(HIPCompiler &&) = delete;
@@ -52,30 +65,49 @@ class HIPCompiler : public Compiler
     HIPCompiler &operator=(const HIPCompiler &) = delete;
 
 #if PIM_COMPILER_ENABLE == 1
-    hipModule_t module_;
+    hipModule_t module_;    ///< Intermediate generated HIP module
 #endif
-    hipFunction_t kernel_;
+    hipFunction_t kernel_;  ///< Generated HIP Function
 };
 
 class HIPDriver : public Driver
 {
    public:
     HIPDriver() = default;
+
+#if PIM_COMPILER_ENABLE == 1
+    /**
+     * @brief function for invoking PIMCompiler and generate
+     * binary and host HIP code
+     * @param output Var designated as output
+     * @param inputs vector of input buffers
+     * @param input_pimbo vector of PIMLibrary input buffers
+     * @param target PimTarget pointer containing target information
+     * @param compile_opts compilation options
+     * @return PimCompiledObj* pointer to generated PIMCompiler object
+     */
+    PimCompiledObj *build_program(pimc::frontend::Var output, std::vector<pimc::frontend::Buffer> inputs,
+                                  std::vector<PimBo *> input_pimbo, PimTarget *target, std::string compile_opts);
+    /**
+     * @brief Function for invoking host HIP compiler and
+     * executing generated code on target
+     * @param obj generated PimCompilerObj
+     * @param target PimTarget pointer containing target information
+     * @param launch_opts execution options
+     * @return PimBo* Output Pimbo
+     */
+    PimBo *execute_program(PimCompiledObj *obj, PimTarget *target, std::string launch_opts);
+#endif
+
+   private:
+    hipFunction_t compile_code_hip(std::string kernel, std::string crf_binary);
+
     HIPDriver(HIPDriver &&) = delete;
     HIPDriver(const HIPDriver &) = delete;
     HIPDriver &operator=(HIPDriver &&) = delete;
     HIPDriver &operator=(const HIPDriver &) = delete;
 
-#if PIM_COMPILER_ENABLE == 1
-    PimCompiledObj *build_program(pimc::frontend::Var output, std::vector<pimc::frontend::Buffer> inputs,
-                                  std::vector<PimBo *> input_pimbo, PimTarget *target, std::string compile_opts);
-    PimBo *execute_program(PimCompiledObj *obj, PimTarget *target, std::string launch_opts);
-#endif
-    // todo:: Pass HW information from user
-
-   private:
-    hipFunction_t compile_code_hip(std::string kernel, std::string crf_binary);
-    HIPCompiler compile_;
+    HIPCompiler compile_; ///< HIPCompiler driver object
 };
 }  // namespace pimc_driver
 }  // namespace runtime
